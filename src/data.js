1eac7f19d179b1e5f859eb34124adf06e167932b
Bohdan Dovhal <46327995+H0us5@users.noreply.github.com>
2023-07-08 18:42:46 +0300

    init

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: "recZkNf2kwmdBcqd0",
+    name: "accent chair",
+    image: "https://www.course-api.com/images/store/product-1.jpeg",
+  },
+  {
+    id: "recEHmzvupvT8ZONH",
+    name: "albany sectional",
+
+    image: "https://www.course-api.com/images/store/product-2.jpeg",
+  },
+  {
+    id: "rec5NBwZ5zCD9nfF0",
+    name: "wooden desk",
+
+    image: "https://www.course-api.com/images/store/product-20.jpeg",
+  },
+  {
+    id: "recd1jIVIEChmiwhe",
+    name: "armchair",
+
+    image: "https://www.course-api.com/images/store/product-4.jpeg",
+  },
+  {
+    id: "recoM2MyHJGHLVi5l",
+    name: "dining table",
+    image: "https://www.course-api.com/images/store/product-5.jpeg",
+  },
+];
+
+export default products;
